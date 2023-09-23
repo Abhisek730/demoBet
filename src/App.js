b1a715352d366eba4870aafabfaea36079296717
Abhishek Agrawal <86093849+Abhisek730@users.noreply.github.com>
2023-09-23 16:25:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Ring from './Ring';
+
+function App() {
+  return (
+    <div className="App">
+     <Ring></Ring>
+    </div>
+  );
+}
+
+export default App;
